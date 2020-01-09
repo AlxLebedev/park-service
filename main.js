4f131c567c9b0670a2ebb15ac2221ca0bd161d0f
Alexey Lebedev <vorresti@ya.ru>
2020-01-09 20:46:57 +0300

    modal links added

---
+++ main.js
@@ -0,0 +1,20 @@
+const modalBtn = document.getElementById('contact-us-button');
+const modalCloseBtn = document.getElementById('modal-close');
+const modal = document.getElementById('modal');
+const modalLinks = Array.from(document.querySelectorAll('.modal-link'));
+
+modalBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    modal.classList.add('modal-active');
+});
+
+modalCloseBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    modal.classList.remove('modal-active');
+});
+
+for (modalLink of modalLinks) {
+    modalLink.addEventListener('click', function() {
+        modal.classList.remove('modal-active');
+    })
+};
